c505ecdf941ab0e9ef1210f67d61006c0d5e427c
git <534368277@qq.com>
2017-06-15 17:41:21 +0800

    third commit

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+/*
+*路由入口
+*/
+module.exprots = function(app){
+  app.get('/', function (req, res) {
+    res.redirect('/posts');
+  });
+  app.use('/signup', require('./signup'));
+  app.use('/signin', require('./signin'));
+  app.use('/signout', require('./signout'));
+  app.use('/posts', require('./posts'));
+};
